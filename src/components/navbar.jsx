f4fc3728fa0818b4a9d0297d4916441e5282d5ca
Mauricio650 <mauricioibanez650@gmail.com>
2025-06-01 11:59:38 -0500

    cambios icon, wepP

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,30 +6,11 @@ export function Navbar () {
     <div className="nav">
     <div className="container">
             <div className="logo">
-                <img src="../public/icons/logoWeb.png" alt="coin-icon" />
+                <img src="../public/icons/coin.webp" alt="coin-icon" />
             </div>
            <a href="#header"> <div className="btn">Home</div></a>
            <a href="#banner"> <div className="btn">Prices</div></a>
            <a href="#footer"> <div className="btn">about</div></a>
-            <svg
-            className="outline"
-            overflow="visible"
-            width="400"
-            height="60"
-            viewBox="0 0 400 60"
-            xmlns="http://www.w3.org/2000/svg"
-            >
-            <rect
-                className="rect"
-                pathLength="100"
-                x="0"
-                y="0"
-                width="400"
-                height="60"
-                fill="transparent"
-                strokeWidth="5"
-            ></rect>
-            </svg>
     </div>
     </div>
     )
